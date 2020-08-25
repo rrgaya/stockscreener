1f505833fc11cc0a7e0178ad198e3a617f23c87c
Ricardo Gaya <rrgaya@gmail.com>
2020-08-25 20:59:55 -0300

    Added model, database connection and schema with pydantic

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from models import Base
+from database import SessionLocal, engine
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
 templates = Jinja2Templates("templates")
+
+
+class StockRequest(BaseModel):
+    symbol: str
+
 
 
 @app.get("/")
@@ -16,11 +27,11 @@ def dashboard(request: Request):
 
 
 @app.post("/stock")
-def create_stock():
+def create_stock(stock_request: StockRequest):
     """ Method to create stock
     """
     return {
         "code": "sucess",
-        "message": "stock created"
+        "message": f"stock {stock_request.symbol} created"
     }
 
