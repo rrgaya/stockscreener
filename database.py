f3abfeeb100e6661f27adc9ceebd9390444ef66e
Ricardo Gaya <rrgaya@gmail.com>
2020-08-27 12:05:27 -0300

    Added new database

--- database.py
+++ database.py
@@ -1,3 +1,4 @@
+import os
 from decouple import config
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -12,9 +13,13 @@ DBNAME = config("DBNAME")
 
 # TODO
 # DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{HOST}:{PORT}/{DBNAME}"
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/stocks"
 
-engine = create_engine(DATABASE_URL)
+if "DATABASE_URL" in os.environ:
+    ENV_DATABASE_URL = "postgres://ynrbxtwqegmste:58feea2c66eac7b672bc088ca20f50153320f9976896a52954af13e8aec90c74@ec2-34-224-229-81.compute-1.amazonaws.com:5432/d35so8n7e5oj21"
+else:
+    ENV_DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/stocks"
+
+engine = create_engine(ENV_DATABASE_URLs)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
